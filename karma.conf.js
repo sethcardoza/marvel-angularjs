11976fc96b7617a5e695211f1047331353cf3f22
Seth Cardoza <seth.cardoza@gmail.com>
2017-10-25 12:58:00 -0400

    starting to setup karma, rearranging some files

---
+++ karma.conf.js
@@ -0,0 +1,25 @@
+// karma.conf.js
+module.exports = function(config) {
+  config.set({
+    files: [
+      'js/**/*.js',
+      'test/**/*.js'
+    ],
+
+    // coverage reporter generates the coverage
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'js/**/*.js': ['coverage']
+    },
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    }
+  });
+};
